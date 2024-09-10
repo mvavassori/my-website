1f385ef622da0fca7474b8abe2050f809bb6299e
Marco Vavassori <mv@marcovavassori.com>
2024-09-10 18:27:29 +0200

    fix main.go formatting

--- main.go
+++ main.go
@@ -21,17 +21,21 @@ type BlogPost struct {
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
 	// Serve about.html on the /about route
 	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./static/about.html")
 	})
 	http.HandleFunc("/", HomeHandler)
-	// http.HandleFunc("/posts/", PostHandler)
 	http.HandleFunc("GET /posts/{slug}", PostHandler)
 	http.HandleFunc("/subscribe", SubscribeHandler)
-	fmt.Println("Starting server at port 8080")
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	port := 8080
+	address := fmt.Sprintf(":%d", port) // :8080
+
+	log.Printf("Server is listening on port %d...\n", port)
+
+	if err := http.ListenAndServe(address, nil); err != nil {
 		log.Fatalf("Failed to start server: %v\n", err)
 	}
 }
